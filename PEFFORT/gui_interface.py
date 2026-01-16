c1b648b78be41e55409cedae8202be55aee0b680
https://github.com/bonvicincoaching-collab <bonvicin.coaching@gmail.com>
2026-01-16 23:16:09 +0100

    updating LICENSE

--- PEFFORT/gui_interface.py
+++ PEFFORT/gui_interface.py
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Copyright (c) 2026 Andrea Bonvicin - bFactor Performance Suite
+# Copyright (c) 2026 Andrea Bonvicin - bFactor Project
 # PROPRIETARY LICENSE - TUTTI I DIRITTI RISERVATI
 # Sharing, distribution or reproduction is strictly prohibited.
 # La condivisione, distribuzione o riproduzione è severamente vietata.
